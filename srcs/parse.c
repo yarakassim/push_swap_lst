1bc0c22d06d547885980bf826ae55555dbe96629
Yara KASSIM-PREMDJEE <ykassim-@e2r4p17.clusters.42paris.fr>
2022-02-18 14:12:16 +0100

    minor changes

--- srcs/parse.c
+++ srcs/parse.c
@@ -6,7 +6,7 @@
 /*   By: ykassim- <ykassim-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 11:49:52 by ykassim-          #+#    #+#             */
-/*   Updated: 2022/02/17 16:44:18 by ykassim-         ###   ########.fr       */
+/*   Updated: 2022/02/18 14:06:39 by ykassim-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,10 @@ int	check_int(char *av)
 {
 	long	n;
 
+	if (ft_strncmp(av, "", 3) == 0)
+		return (0);
+	if (ft_strlen(av) > 11)
+		return (0);
 	n = ft_atoi(av);
 	if (!ft_isdigit2(av))
 		return (0);
@@ -57,7 +61,7 @@ int	check_doubles(char **av)
 		i = c;
 		while (av[++i])
 		{
-			if (!ft_strncmp(av[c], av[i], 10))
+			if (ft_strncmp(av[c], av[i], 11) == 0)
 				return (0);
 		}
 	}
